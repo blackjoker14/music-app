0d0a64e2e81bb777196a9fac4f597db7c63c8f53
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2022-11-19 22:20:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 song = "";
+leftWristY = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristX = 0;
+
+
+
 
 function preload(){
     song = loadSound("happy-day-113985.mp3")
@@ -10,6 +17,25 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, mobelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modeLoaded(){
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results) {
+    if(results.lenth > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX = " + leftWristX + "  leftWristY = " + leftWristY);
+        console.log("rightWristX = " + rightWristX + "  rightWrist = " + rightWristY);
+    }
 }
 
 function draw(){
@@ -18,4 +44,6 @@ function draw(){
 
 function play(){
   song.play();
+  song.setVolmne(1);
+  song.rate(1);
 }
